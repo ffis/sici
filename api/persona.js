45a53accccfa21824575fb27286d81789ff55c3f
Ivan <ivanlorca@gmail.com>
2014-10-29 14:43:30 +0100

    gestión permisos. Implementación parcial

--- api/persona.js
+++ api/persona.js
@@ -23,17 +23,17 @@ exports.personassearchlist = function(models,Q)
 		var defer_persona = Q.defer();
 		var defer_procedimiento = Q.defer();
 		
-		console.log("Buscando personas...");
+
 		/// 1. Buscamos personas en la tabla personas.
 		Persona.find({},{codplaza:true,login:true,nombre:true,apellidos:true},function(err,data){
 			if (err) {
 				console.error(err); res.status(500); res.end(); defer_persona.reject(err);		
 			} else {
-				console.log("Buscando personas...2");
+
 				defer_persona.resolve(data);
 			}			
 		});
-		console.log("Buscando personas...1");
+
 		/// 2. Buscamos personas como responsables de procedimientos ... ¡¡¡Y que no estén en el primer grupo¡¡¡
 		Procedimiento.aggregate()
 			.unwind("responsables")
@@ -48,7 +48,7 @@ exports.personassearchlist = function(models,Q)
 				if (err) {
 					console.error(err); res.status(500); res.end(); defer_procedimiento.reject(err); 			
 				} else {
-					console.log("Buscando personas...3");
+
 					defer_procedimiento.resolve(data);
 				}
 			});
@@ -62,18 +62,18 @@ exports.personassearchlist = function(models,Q)
 				var persona = personas_by_persona[i];
 				var idr = persona.login+"-"+persona.codplaza;
 				r[idr] = persona;			
-				response.push( persona.login+" ; "+persona.codplaza+" ; "+
-							persona.nombre+" "+persona.apellidos);
+				response.push( {data: persona.login+" ; "+persona.codplaza+" ; "+
+							persona.nombre+" "+persona.apellidos});
 			}
 			for(var i=0;i<personas_by_responsable.length;i++){
 				var persona = personas_by_responsable[i];
 				var idr = persona._id.login + persona.codplaza;
 				if (typeof r[idr] === 'undefined') {
 					r[idr] = persona[i];
-					response.push(persona._id.login+" ; "+persona._id.codplaza+" ; "+persona.nombre+" "+persona.apellidos);
+					response.push({data:persona._id.login+" ; "+persona._id.codplaza+" ; "+persona.nombre+" "+persona.apellidos});
 				}
 			}
-			console.log("Buscando personas...4");
+
 			res.json(response);
 		});
 	}
--- public/js/controllers/permisos.js
+++ public/js/controllers/permisos.js
@@ -1,5 +1,5 @@
 
-function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosList) {
+function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosList,PersonasSearchList) {
 	$rootScope.nav = 'permisos';
 	$window.document.title ='SICI: Permisos';
 
@@ -8,8 +8,18 @@ function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosL
 	$scope.is_show_recursive_users = false;
 	$scope.show_responsables = true;
 	$scope.show_form_add_permiso = false;
+	$scope.personas_plazas = [];
+	$scope.persona_o_plaza = null;
+	$scope.show_details_permiso = false;
 	
-	
+	$scope.clearaddpermisoform = function(){
+		$scope.logincarm = "";
+		$scope.plaza = "";
+	}
+	$scope.refreshpermisoform = function(){
+		$scope.logincarm = persona_o_plaza.login;
+		$scope.plaza = persona_o_plaza.plaza;
+	}
 	$scope.show_recursive_users = function(){
 		$scope.is_show_recursive_users = true;
 		$scope.setSeleccionado($scope.seleccionado);		
@@ -18,8 +28,19 @@ function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosL
 		$scope.is_show_recursive_users = false;
 		$scope.setSeleccionado($scope.seleccionado);
 	}
+	$scope.crearpermiso = function() {
+		if ($scope.logincarm.replace(/^\s+|\s+$/gm,'') || $scope.plaza.replace(/^\s+|\s+$/gm,''))
+		{
+			
+		}
+	}
 	
-
+	$scope.showDetailsPermiso = function(){
+		$scope.show_details_permiso = true;
+		$scope.seleccionado = false;
+	}
+	
+	$scope.clearaddpermisoform();
 	$scope.jerarquia = $rootScope.session.permisoscalculados.jerarquialectura;
 	$scope.superuser = $rootScope.session.permisoscalculados.superuser;
 	
@@ -46,6 +67,8 @@ function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosL
 				console.log($scope.permisostotales);
 				$scope.cumplimentados = 0;
 				$scope.count = 1;
+				$scope.persona_o_plaza='';
+				$scope.show_details_permiso=false;
 			}
 		};
 		
@@ -74,204 +97,19 @@ function PermisoCtrl($rootScope,$scope,$location,$window,Arbol,Session,PermisosL
 
 	$scope.addPermiso = function() {
 		if ($scope.seleccionado !== 'undefined') {
-			
+			$scope.show_form_add_permiso = true;			
 		}
 	}
-    
-	/*
 	
-	$scope.sparkline = function(){
-		setTimeout( function(){	$('.sparkline').each(function(){
-				var obj = $(this).html();
-				try{
-					var t = JSON.parse( obj );
-					$(this).sparkline( t, {type: 'bar',barColor: '#a94442'}) ; 
-				}catch(e){
-					
-				}
-			});
-		},1);
-	}
-	$scope.filtrotxtprocedimiento = {};
-	$scope.$watch('filtrotxtprocedimiento.$',function(newValue,oldValue){ $scope.sparkline(); });
-	$scope.$watch('procedimientosocultos',function(newValue,oldValue){ $scope.sparkline(); });
-	//$scope.$watch('oculto', function(){setTimeout(function(){nv.utils.windowResize(); }, 10 ); });
-
-	$scope.procedimientosfiltrados = [];
-	$scope.$watch('filtro',function(newValue,oldValue){
-		var result = [];
-		$scope.procedimientos.forEach(function(p,j){
-			var ok = true;
-			for(var campofiltro in $scope.filtro){
-				if ($scope.filtro[campofiltro]!='TODOS' && p[campofiltro] != $scope.filtro[campofiltro]){
-					ok=false; break;
-				}
-			}
-			if (ok)
-				result.push(p);
-			else
-				console.info(p);
-		});
-		$scope.procedimientosfiltrados = result;
-
-		$scope.setProcedimiento ( ($scope.procedimientosfiltrados.length>0) ?  $scope.procedimientosfiltrados[0] : false );
-		$scope.sparkline();
-		$scope.procedimientosocultos = false;
-	}, true);
-
-
-	$scope.xAxisTickValuesFunction = function(){ return function(d){ return [0,1,2,3,4,5,6,7,8,9,10,11]; };};
-	$scope.xAxisTickFormatFunction = function(){ return function(d){ return $scope.meses[d]; } };
-	$scope.colorFunction= function(){ return function(d,i){ 
-		var color = $scope.colorText(i, 5, 60);
-		var r = (color.red<16 ? '0': '')+color.red.toString(16), g = (color.green<16 ? '0': '')+color.green.toString(16), b = (color.blue<16 ? '0': '')+color.blue.toString(16);
-	 	return '#'+r+g+b;
-	}};
-
-	$scope.graphs = false;
-	$scope.$watch('procedimientoSeleccionado',function(newvalue,oldvalue){
-		if (newvalue){
-			
-			var graphskeys = [
-					{caption:'RESUMEN DE DATOS DE GESTIÓN',keys:['Solicitados','Iniciados','Pendientes','Total resueltos']},
-					{caption:'RESUELTOS EN PLAZO',keys:['En plazo','Fuera de plazo']},
-					{caption:'DESESTIMIENTOS/RENUNCIAS Y PRESCRITOS/CADUCADOS',keys:['Resueltos por Desistimiento/Renuncia/Caducidad (Resp_Ciudadano)',	'Resueltos por Prescripcion/Caducidad (Resp_Admon)']},
-					{caption:'QUEJAS Y RECURSOS CONTRA EL PROCEDIMIENTO',keys:['Quejas presentadas en el mes','Recursos presentados en el mes']},
-					{caption:'VARIACIÓN DE TRAMITACIÓN MENSUAL (DATOS ABSOLUTOS)',keys:['Tramitados 2013','Total resueltos']},
-				];
-			$scope.graphs = [];
-			graphskeys.forEach(function(g, i){
-				var maxvalue = 0;
-				var data = [];
-				var caption = g.caption;
-				g.keys.forEach(function(key,indx){
-					var values = [];
-					if ($scope.procedimientoSeleccionado[key]){
-						$scope.procedimientoSeleccionado[key].forEach(function (val,idx) {
-							values.push( [ idx, val] ) ;
-							if (maxvalue< val) maxvalue=val;
-						});
-						data.push(	{ "key": key,"values": values} );
-					}
-				})
-				var forcey = [0, Math.ceil(maxvalue*1.3) ];
-				if (maxvalue>0)
-					$scope.graphs.push( { data : data, forcey : forcey, caption:caption} );
-			})
-		}else{
-			$scope.graphs = false;
-		}
-
-	})
-	$scope.cumplimentados = 0;
-	$scope.count = 1;
-	$scope.$watch('procedimientos.$resolved', function(newValue, oldValue) {
-
-		$scope.setProcedimiento ( ($scope.procedimientos.length>0) ?  $scope.procedimientos[0] : false );
-		$scope.procedimientosfiltrados = $scope.procedimientos;
-		if (newValue && $scope.procedimientos.length>0)
+	var personas_plazas_aux = PersonasSearchList.query({},function(err,data){
+		$scope.personas_plazas=[];
+		for(var i=0;i<personas_plazas_aux.length;i++)
 		{
-			$scope.responsables = {};
-			$scope.filtros = {};
-			$scope.filtro = {};
-			$scope.cumplimentados = 0;
-			$scope.count = $scope.procedimientos.length;
-//console.log('50. procedimientos.length='+$scope.procedimientos.length+ ' procedimientosfiltrados.length='+$scope.procedimientosfiltrados.length)
-			$scope.procedimientos.forEach(function(p){
-				if (p.codigo < $scope.procedimientoSeleccionado.codigo)
-					$scope.procedimientoSeleccionado = p;
-				var cumplimentado = $scope.cumplimentado(p);
-				cumplimentado && $scope.cumplimentados++;
-				for(var i in $scope.camposfiltros){
-					var campofiltro = $scope.camposfiltros[i], value = p[campofiltro], name  = p[campofiltro], count = 1;
-					if (typeof $scope.filtros[campofiltro] === 'undefined')
-						$scope.filtros[campofiltro] = {};
-					if (typeof $scope.filtros[campofiltro][value] === 'undefined')
-					{
-						$scope.filtros[campofiltro][value] = { name: name, value:value, count:count, cumplimentados:cumplimentado ? 1 : 0}
-					}else{
-						$scope.filtros[campofiltro][value].count = $scope.filtros[campofiltro][value].count+1;
-						if (cumplimentado)
-							$scope.filtros[campofiltro][value].cumplimentados = $scope.filtros[campofiltro][value].cumplimentados+1;
-					}
-					$scope.filtros[campofiltro][value].name = $scope.filtros[campofiltro][value].value + ' ('+($scope.filtros[campofiltro][value].cumplimentados)+'/'+($scope.filtros[campofiltro][value].count)+')';
-				}
-			});
-			
-			for(var i in $scope.camposfiltros){
-				var campofiltro = $scope.camposfiltros[i];
-				if (Object.keys($scope.filtros[campofiltro]).length > 1)
-				{
-					$scope.filtros[campofiltro].TODOS = { name:'TODOS',value:'TODOS', count:0};
-					$scope.filtro[campofiltro] = 'TODOS';
-				}else{
-					for(var a in $scope.filtros[campofiltro])
-						$scope.filtro[campofiltro] = $scope.filtros[campofiltro][a].value;
-					//console.log(campofiltro);
-				}
-			}
-
-			if (Object.keys($scope.responsables).length > 1)
-			{
-				$scope.responsables.TODOS = { name:'TODOS',value:'TODOS', count:0};
-				$scope.responsable = $scope.responsables.TODOS;
-			}else{
-				for(var a in $scope.responsables)
-					$scope.responsable = $scope.responsables[a];
-			}
-
-			
-			$scope.sparkline();
-			
+			$scope.personas_plazas.push(personas_plazas_aux[i].data);
 		}
 	});
-	$scope.anualidad = new Date().getFullYear();
-	$scope.camposfiltros = ['Denominacion Nivel 3','Denominacion Nivel 2','Denominacion Nivel 1', 'Nombre responsable',];
-	$scope.filtros = {};
-	$scope.filtro = {};
-	$scope.camporesponsable = 'Nombre responsable';
-	$scope.responsables = {};
-	$scope.procedimientosocultos = false;
-	$scope.attrspar = [
-		'codigo',
-		'denominacion',
-		'Codigo Nivel 1',
-		'Denominacion Nivel 1',
-		'Codigo Nivel 2',
-		'Denominacion Nivel 2',
-		'Codigo Nivel 3',
-		'Denominacion Nivel 3',
-		'Codigo plaza responsable',
-		'Login responsable',
-		'Nombre responsable',
-		'Correo-e responsable',
-		'Teléfono responsable',
-		'Plazo maximo legal para resolver (dias naturales)',
-		'Plazo maximo legal para responder (dias habiles)',
-		'Plazo CS /ANS (dias naturales)',
-		'Plazo CS /ANS (dias habiles)',
-		'Pendientes iniciales (a 31-12)',
-	];
-	$scope.attrstabla = ['Tramitados 2013','Solicitados',
-		'Iniciados',
-		'Resueltos [1]',
-		'Resueltos [5]',
-		'Resueltos [10]',
-		'Resueltos [15]',
-		'Resueltos [30]',
-		'Resueltos [45]',
-		'Resueltos [>45]',
-		'Resueltos por Desistimiento/Renuncia/Caducidad (Resp_Ciudadano)',
-		'Resueltos por Prescripcion/Caducidad (Resp_Admon)',
-		'T_ medio dias naturales',
-		'T_ medio dias habiles descontando T_ de suspensiones',
-		'En plazo',
-		'Quejas presentadas en el mes',
-		'Recursos presentados en el mes',
-		'Total resueltos',
-		'Fuera de plazo',
-		'Pendientes',
-		];
-	$scope.meses = $rootScope.meses;*/
+	
+    
+	
 }
-PermisoCtrl.$inject = ['$rootScope','$scope','$location','$window','Arbol','Session','PermisosList'];
+PermisoCtrl.$inject = ['$rootScope','$scope','$location','$window','Arbol','Session','PermisosList','PersonasSearchList'];
