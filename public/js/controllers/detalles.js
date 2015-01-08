263abb2f70c7a9c771154be2042e21ffcf350bb9
asniaire <asniaire@gmail.com>
2015-01-08 08:48:52 +0100

    Arreglar graficas

--- public/js/controllers/detalles.js
+++ public/js/controllers/detalles.js
@@ -13,17 +13,21 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
 
 
     $scope.nextYear = function () {
-        $scope.anualidad = 'a' + (parseInt($scope.anualidad.substring(1, 5)) + 1);
+        var year = parseInt($scope.anualidad.substring(1, 5)) + 1;
+        $scope.anualidad = 'a' + (year);
+        $scope.updateGraphKeys(year);
     };
 
     $scope.prevYear = function () {
-        $scope.anualidad = 'a' + (parseInt($scope.anualidad.substring(1, 5)) - 1);
+        var year = parseInt($scope.anualidad.substring(1, 5)) - 1;
+        $scope.anualidad = 'a' + (year);
+        $scope.updateGraphKeys(year);
     };
 
     $scope.exists = function (attr) {
         if ($scope.anualidad) {
             return $scope.procedimientoSeleccionado.periodos[$scope.anualidad] &&
-                    typeof $scope.procedimientoSeleccionado.periodos[$scope.anualidad][attr] != 'undefined';
+                    typeof $scope.procedimientoSeleccionado.periodos[$scope.anualidad][attr] !== 'undefined';
         }
     };
 
@@ -97,9 +101,75 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
         });
         $scope.nombrePadre = 'Sin definir';
     };
+    
+    $scope.updateGraphKeys = function(anualidad) {
+        var anualidad_anterior = anualidad-1;
+        var graphskeys = [
+            {caption: 'RESUMEN DE DATOS DE GESTIÓN ' + anualidad, keys: [
+                    {caption: 'Solicitados', vals: 'periodos.a' + anualidad + '.solicitados', maxx: $scope.mesActual},
+                    {caption: 'Iniciados', vals: 'periodos.a' + anualidad + '.iniciados', maxx: $scope.mesActual},
+                    {caption: 'Pendientes', vals: 'periodos.a' + anualidad + '.pendientes', maxx: $scope.mesActual},
+                    {caption: 'Total resueltos', vals: 'periodos.a' + anualidad + '.total_resueltos', maxx: $scope.mesActual},
+                    {caption: 'Total resueltos ' + anualidad_anterior, vals: 'periodos.a' + anualidad_anterior + '.total_resueltos', maxx: 12}
+                ]},
+            {caption: 'RESUELTOS EN PLAZO ' + anualidad, keys: [
+                    {caption: 'En plazo', vals: 'periodos.a' + anualidad + '.en_plazo', maxx: $scope.mesActual},
+                    {caption: 'Fuera de plazo', vals: 'periodos.a' + anualidad + '.fuera_plazo', maxx: $scope.mesActual}
+                ]},
+            {caption: 'DESESTIMIENTOS/RENUNCIAS Y PRESCRITOS/CADUCADOS ' + anualidad, keys: [
+                    {caption: 'Resueltos por Desistimiento/Renuncia/Caducidad (Resp_Ciudadano)', vals: 'periodos.a' + anualidad + '.resueltos_desistimiento_renuncia_caducidad', maxx: $scope.mesActual},
+                    {caption: 'Resueltos por Prescripcion/Caducidad (Resp_Admon)', vals: 'periodos.a' + anualidad + '.resueltos_prescripcion', maxx: $scope.mesActual}
+                ]},
+            {caption: 'QUEJAS Y RECURSOS CONTRA EL PROCEDIMIENTO ' + anualidad, keys: [
+                    {caption: 'Quejas presentadas en el mes', vals: 'periodos.a' + anualidad + '.quejas', maxx: $scope.mesActual},
+                    {caption: 'Recursos presentados en el mes', vals: 'periodos.a' + anualidad + '.recursos', maxx: $scope.mesActual}
+                ]}
+
+        ];
+        $scope.graphskeys = graphskeys;
+        $scope.graphs = [];
+        $scope.numgraphs = 0;
+        graphskeys.forEach(function (g, i) {
+            var maxvalue = 0;
+            var data = [];
+            var caption = g.caption;
+            g.keys.forEach(function (key, indx) {
+                var k = $scope.procedimientoSeleccionado;
+                var values = [];
+                var indexes = key.vals.split('.');
+                for (var j in indexes) {
+                    var index = indexes[j];
+                    if (typeof k[index] === 'undefined')
+                        break;
+                    k = k[index];
+                }
+                if (typeof k !== 'undefined' && k.length > 0) {
+                    k.forEach(function (val, idx) {
+                        /*try{
+                         console.log(idx +':'+graphskeys[i].keys[indx].maxx);
+                         }catch(e){
+                         console.error(i+':'+indx);
+                         }*/
+                        if (((idx <= graphskeys[i].keys[indx].maxx) && (anualidad === $scope.anualidadActual)) || (anualidad !== $scope.anualidadActual)) {
+                            values.push([idx, val]);
+                            if (maxvalue < val)
+                                maxvalue = val;
+                        }
+                    });
+                    data.push({"key": key.caption, "values": values});
+                } else {
+                    console.log('Index malo:' + indexes);
+                }
+            });
+            var forcey = [0, Math.ceil(maxvalue * 1.3)];
+            if (maxvalue > 0) {
+                $scope.graphs.push({data: data, forcey: forcey, caption: caption});
+                $scope.numgraphs = $scope.numgraphs + 1;
+            }
+        });
+    };
 
     $scope.procedimientoSeleccionado = Procedimiento.get({codigo: $routeParams.codigo}, function () {
-      
         $window.document.title = 'SICI: ' + $scope.procedimientoSeleccionado.denominacion;
         $rootScope.procedimiento = $scope.procedimientoSeleccionado.codigo;
         $scope.anualidad = '000000';
@@ -121,8 +191,10 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
 
         for (var anualidad in $scope.procedimientoSeleccionado.periodos) {
             console.log(anualidad);
-            if (parseInt(anualidad.substring(1, 5)) > parseInt($scope.anualidad.substring(1, 5)))
+            if (parseInt(anualidad.substring(1, 5)) > parseInt($scope.anualidad.substring(1, 5))) {
                 $scope.anualidad = anualidad;
+                $scope.anualidadActual = anualidad.substring(1,5);
+            }
         }
         if ($scope.procedimientoSeleccionado.ancestros && $scope.procedimientoSeleccionado.ancestros[0].id == 1)
         {
@@ -141,73 +213,8 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
         });
 
         $scope.superuser = $rootScope.superuser();
-
-        var cod_plaza = $scope.procedimientoSeleccionado.cod_plaza;
-        var anualidad_anterior = parseInt($scope.anualidad.substring(1,5))-1;
-        var graphskeys = [
-            {caption: 'RESUMEN DE DATOS DE GESTIÓN ' + $scope.anualidad.substring(1, 5), keys: [
-                    {caption: 'Solicitados', vals: 'periodos.' + $scope.anualidad + '.solicitados', maxx: $scope.mesActual},
-                    {caption: 'Iniciados', vals: 'periodos.' + $scope.anualidad + '.iniciados', maxx: $scope.mesActual},
-                    {caption: 'Pendientes', vals: 'periodos.' + $scope.anualidad + '.pendientes', maxx: $scope.mesActual},
-                    {caption: 'Total resueltos', vals: 'periodos.' + $scope.anualidad + '.total_resueltos', maxx: $scope.mesActual},
-                    {caption: 'Total resueltos ' + anualidad_anterior, vals: 'periodos.a' + anualidad_anterior + '.total_resueltos', maxx: 12},
-                ]},
-            {caption: 'RESUELTOS EN PLAZO ' + $scope.anualidad.substring(1, 5), keys: [
-                    {caption: 'En plazo', vals: 'periodos.' + $scope.anualidad + '.en_plazo', maxx: $scope.mesActual},
-                    {caption: 'Fuera de plazo', vals: 'periodos.' + $scope.anualidad + '.fuera_plazo', maxx: $scope.mesActual},
-                ]},
-            {caption: 'DESESTIMIENTOS/RENUNCIAS Y PRESCRITOS/CADUCADOS ' + $scope.anualidad.substring(1, 5), keys: [
-                    {caption: 'Resueltos por Desistimiento/Renuncia/Caducidad (Resp_Ciudadano)', vals: 'periodos.' + $scope.anualidad + '.resueltos_desistimiento_renuncia_caducidad', maxx: $scope.mesActual},
-                    {caption: 'Resueltos por Prescripcion/Caducidad (Resp_Admon)', vals: 'periodos.' + $scope.anualidad + '.resueltos_prescripcion', maxx: $scope.mesActual},
-                ]},
-            {caption: 'QUEJAS Y RECURSOS CONTRA EL PROCEDIMIENTO ' + $scope.anualidad.substring(1, 5), keys: [
-                    {caption: 'Quejas presentadas en el mes', vals: 'periodos.' + $scope.anualidad + '.quejas', maxx: $scope.mesActual},
-                    {caption: 'Recursos presentados en el mes', vals: 'periodos.' + $scope.anualidad + '.recursos', maxx: $scope.mesActual},
-                ]},
-
-        ];
-        $scope.graphskeys = graphskeys;
-        $scope.graphs = [];
-        $scope.numgraphs = 0;
-        graphskeys.forEach(function (g, i) {
-            var maxvalue = 0;
-            var data = [];
-            var caption = g.caption;
-            g.keys.forEach(function (key, indx) {
-                var k = $scope.procedimientoSeleccionado;
-                var values = [];
-                var indexes = key.vals.split('.');
-                for (var j in indexes) {
-                    var index = indexes[j];
-                    if (typeof k[index] == 'undefined')
-                        break;
-                    k = k[index];
-                }
-                if (typeof k != 'undefined' && k.length > 0) {
-                    k.forEach(function (val, idx) {
-                        /*try{
-                         console.log(idx +':'+graphskeys[i].keys[indx].maxx);
-                         }catch(e){
-                         console.error(i+':'+indx);
-                         }*/
-                        if (idx < graphskeys[i].keys[indx].maxx) {
-                            values.push([idx, val]);
-                            if (maxvalue < val)
-                                maxvalue = val;
-                        }
-                    });
-                    data.push({"key": key.caption, "values": values});
-                } else {
-                    console.log('Index malo:' + indexes);
-                }
-            });
-            var forcey = [0, Math.ceil(maxvalue * 1.3)];
-            if (maxvalue > 0) {
-                $scope.graphs.push({data: data, forcey: forcey, caption: caption});
-                $scope.numgraphs = $scope.numgraphs + 1;
-                console.log(data);
-            }
-        });
+        $scope.updateGraphKeys($scope.anualidad.substring(1,5));
+        
 
         $scope.inconsistencias = Raw.query({model: 'reglasinconsistencias'}, function () {
             $scope.inconsistencias.forEach(function (i, idx) {
@@ -396,7 +403,7 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
         'resueltos_1', 'resueltos_5', 'resueltos_10', 'resueltos_15', 'resueltos_30', 'resueltos_45', 'resueltos_mas_45',
         'resueltos_desistimiento_renuncia_caducidad', 'resueltos_prescripcion',
         'en_plazo',
-        't_medio_habiles', 't_medio_naturales',
+        't_medio_habiles', 't_medio_naturales'
     ];
     /* 'totalsolicitudes', */
     $scope.attrstablacalculados = ['total_resueltos', 'fuera_plazo', 'pendientes'];
@@ -432,6 +439,7 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
     $scope.recalculate = function (force) {
         if (force || $scope.cellChanged) {
             $scope.procedimientoSeleccionado.$update(function (response) {
+                $scope.updateGraphKeys($scope.anualidad.substring(1,5));
                 console.error(response);
             });
         }
@@ -450,46 +458,46 @@ function DetallesCtrl($q, $rootScope, $scope, $routeParams, $window, $location, 
         }
     };
 
-    $scope.downloadGraphic = function (id) {
-        console.log(id);
-        var canvas = document.getElementById(id);
-        var ctx = canvas.getContext("2d");
-        // draw to canvas...
-        canvas.toBlob(function (blob) {
-            saveAs(blob, "image.png");
-        });
-    };
-    
-    $scope.guardarImagen = function(id) {
-//        http://techslides.com/save-svg-as-an-image
-      var html = d3.select('#svg'+id)
-        .attr("version", 1.1)
-        .attr("xmlns", "http://www.w3.org/2000/svg")
-        .node().parentNode.innerHTML;
- 
-        console.log(html);
-        var imgsrc = 'data:image/svg+xml;base64,'+ btoa(html);
-        var img = '<img src="'+imgsrc+'">'; 
-        d3.select("#svgdataurl"+id).html(img);
-
-
-        var canvas = document.querySelector("canvas"),
-                context = canvas.getContext("2d");
-
-        var image = new Image();
-        image.src = imgsrc;
-        image.onload = function() {
-            context.drawImage(image, 0, 0);
-            var canvasdata = canvas.toDataURL("image/png");
-            var pngimg = '<img src="'+canvasdata+'">'; 
-            d3.select("#pngdataurl"+id).html(pngimg);
-
-            var a = document.createElement("a");
-            a.download = "grafica.png";
-            a.href = canvasdata;
-            a.click();
-        };  
-    };
+//    $scope.downloadGraphic = function (id) {
+//        console.log(id);
+//        var canvas = document.getElementById(id);
+//        var ctx = canvas.getContext("2d");
+//        // draw to canvas...
+//        canvas.toBlob(function (blob) {
+//            saveAs(blob, "image.png");
+//        });
+//    };
+//    
+//    $scope.guardarImagen = function(id) {
+////        http://techslides.com/save-svg-as-an-image
+//      var html = d3.select('#svg'+id)
+//        .attr("version", 1.1)
+//        .attr("xmlns", "http://www.w3.org/2000/svg")
+//        .node().parentNode.innerHTML;
+// 
+//        console.log(html);
+//        var imgsrc = 'data:image/svg+xml;base64,'+ btoa(html);
+//        var img = '<img src="'+imgsrc+'">'; 
+//        d3.select("#svgdataurl"+id).html(img);
+//
+//
+//        var canvas = document.querySelector("canvas"),
+//                context = canvas.getContext("2d");
+//
+//        var image = new Image();
+//        image.src = imgsrc;
+//        image.onload = function() {
+//            context.drawImage(image, 0, 0);
+//            var canvasdata = canvas.toDataURL("image/png");
+//            var pngimg = '<img src="'+canvasdata+'">'; 
+//            d3.select("#pngdataurl"+id).html(pngimg);
+//
+//            var a = document.createElement("a");
+//            a.download = "grafica.png";
+//            a.href = canvasdata;
+//            a.click();
+//        };  
+//    };
     
     $scope.descargarExcel = function() {
         ExportarResultadosProcedimiento.get({codigo: $scope.procedimientoSeleccionado.codigo, year: $scope.anualidad}, function (token) {
