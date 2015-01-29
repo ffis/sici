7319ecdaaa1b12de4814cb32071ae263ac450d5e
Mario Loksly <loksly@gmail.com>
2015-01-29 14:45:43 +0100

    eslint optimization

--- public/js/controllers/etiqueta.js
+++ public/js/controllers/etiqueta.js
@@ -1,27 +1,33 @@
-function EtiquetaCtrl($rootScope,$scope,$window, $routeParams,Etiqueta) {
-	$rootScope.nav = 'etiqueta';
-	$window.document.title ='SICI: Etiquetas';
-	$scope.cambios = [];
-	$scope.etiquetas =  Etiqueta.query();
-	$scope.nuevo =  new Etiqueta();
-	$scope.actualizar = function(regla){
-		regla.$update(function(){
-			$scope.cambios = [];
-		});
-	};
-	$scope.eliminar = function(regla){
-		if (confirm('¿Está seguro? Esta operación no es reversible.'))
-		regla.$delete(function(){
-			$scope.cambios = [];
-			$scope.etiquetas =  Etiqueta.query();
-		});
-	};
-	$scope.guardar = function(regla){
-		Etiqueta.save($scope.nuevo, function() {
-			$scope.etiquetas = Etiqueta.query();
-			$scope.nuevo = new Etiqueta();
-		});
-	}
-}
-
-EtiquetaCtrl.$inject = ['$rootScope','$scope','$window', '$routeParams','Etiqueta'];
+(function(angular, confirm){
+	'use strict';
+	angular.module('sici')
+		.controller('EtiquetaCtrl', [ '$rootScope', '$scope', '$window', '$routeParams', 'Etiqueta',
+			function ($rootScope, $scope, $window, $routeParams, Etiqueta) {
+				$rootScope.nav = 'etiqueta';
+				$window.document.title = 'SICI: Etiquetas';
+				$scope.cambios = [];
+				$scope.etiquetas = Etiqueta.query();
+				$scope.nuevo = new Etiqueta();
+				$scope.actualizar = function(regla){
+					regla.$update(function(){
+						$scope.cambios = [];
+					});
+				};
+				$scope.eliminar = function(regla){
+					if (confirm('¿Está seguro? Esta operación no es reversible.'))
+					{
+						regla.$delete(function(){
+							$scope.cambios = [];
+							$scope.etiquetas = Etiqueta.query();
+						});
+					}
+				};
+				$scope.guardar = function(){
+					Etiqueta.save($scope.nuevo, function() {
+						$scope.etiquetas = Etiqueta.query();
+						$scope.nuevo = new Etiqueta();
+					});
+				};
+			}
+		]);
+})(angular, confirm);
--- public/js/controllers/welcome.js
+++ public/js/controllers/welcome.js
@@ -1,42 +1,45 @@
+(function(angular){
+	'use strict';
+	angular.module('sici')
+		.controller('WelcomeCtrl', [ '$rootScope', '$scope', '$window', 'Aggregate', 'ProcedimientoCount', 'TramitesCount', 'PorcentajeTramitesResultos', 'ProcedimientosSinExpedientes',
+			function ($rootScope, $scope, $window, Aggregate, ProcedimientoCount, TramitesCount, PorcentajeTramitesResultos, ProcedimientosSinExpedientes){
 
+				$rootScope.nav = 'inicio';
+				$window.document.title = 'SICI - Portada';
 
-function WelcomeCtrl($rootScope,$scope,$window,Aggregate,ProcedimientoCount,TramitesCount,PorcentajeTramitesResultos,ProcedimientosSinExpedientes){
-    $rootScope.nav = 'inicio';
-    $window.document.title ='SICI - Portada';
+				$scope.indicadores = [ ];
+				$scope.welcomeDetailsEnabled = false;
 
-    $scope.indicadores = [ ];
-    $scope.welcome_detailsenabled = false;
+				if ($scope.welcomeDetailsEnabled)
+				{
+					$scope.procedimientos = ProcedimientoCount.get(function(){
+						$scope.indicadores.push( { bg: 'red-soft', descripcion: 'Procedimientos', number: $scope.procedimientos.count, icon: 'fa-folder-open'});
+					});
+					$scope.tramites = TramitesCount.get(function(){
+						$scope.indicadores.push( { bg: 'green-soft', descripcion: 'Expedientes', number: $scope.tramites.suma, icon: 'fa-comments'});
+					});
+					$scope.ratio = PorcentajeTramitesResultos.get(function(){
+						$scope.indicadores.push( { bg: 'blue-soft', descripcion: 'Cumplimiento', number: ($scope.ratio.ratio * 100) + '%', icon: 'fa-pie-chart'});
+					});
+					$scope.sinexpediente = ProcedimientosSinExpedientes.get(function(){
+						$scope.indicadores.push( { bg: 'purple-soft', descripcion: 'Procedimientos sin expedientes', number: $scope.sinexpediente.total, icon: 'fa-bar-chart'});
+					});
+				}
 
-    if ($scope.welcome_detailsenabled)
-    {
-        $scope.procedimientos = ProcedimientoCount.get(function(){
-            $scope.indicadores.push( {bg:'red-soft',descripcion:'Procedimientos',number:$scope.procedimientos.count,icon:'fa-folder-open'});
-        });
-        $scope.tramites = TramitesCount.get(function(){
-            $scope.indicadores.push( {bg:'green-soft',descripcion:'Expedientes',number:$scope.tramites.suma,icon:'fa-comments'});
-        });
-        $scope.ratio= PorcentajeTramitesResultos.get(function(){
-            $scope.indicadores.push( {bg:'blue-soft',descripcion:'Cumplimiento',number:($scope.ratio.ratio*100)+'%',icon:'fa-pie-chart'});
-        });
-        $scope.sinexpediente= ProcedimientosSinExpedientes.get(function(){
-            $scope.indicadores.push( {bg:'purple-soft',descripcion:'Procedimientos sin expedientes',number:$scope.sinexpediente.total,icon:'fa-bar-chart'});
-        });
-    }
+				var date = new Date();
+				$scope.mesActual = date.getMonth();
+				$scope.anyoActual = date.getFullYear();
 
-    var date = (new Date());
-    $scope.mesActual  = date.getMonth();
-    $scope.anyoActual = date.getFullYear();
-    
-    $scope.pendientes = Aggregate.query({
-            anualidad : $scope.anyoActual,
-            campo: JSON.stringify({'codigo':'$codigo','denominacion':'$denominacion'}),
-            restriccion: "{\"periodos.a"+$scope.anyoActual+".totalsolicitudes\":{\"$lt\":1}}"
-    });
-    $scope.inconsistencias = Aggregate.query({
-            anualidad : $scope.anyoActual,
-            campo: JSON.stringify({'codigo':'$codigo','denominacion':'$denominacion'}),
-            restriccion: "{\"periodos.a"+$scope.anyoActual+".pendientes\":{\"$lt\":0}}"
-    });
-}
-
-WelcomeCtrl.$inject =  ['$rootScope','$scope','$window','Aggregate','ProcedimientoCount', 'TramitesCount', 'PorcentajeTramitesResultos', 'ProcedimientosSinExpedientes'];
+				$scope.pendientes = Aggregate.query({
+						anualidad: $scope.anyoActual,
+						campo: JSON.stringify({'codigo': '$codigo', 'denominacion': '$denominacion'}),
+						restriccion: "{\"periodos.a" + $scope.anyoActual + ".totalsolicitudes\":{\"$lt\":1}}"
+				});
+				$scope.inconsistencias = Aggregate.query({
+						anualidad: $scope.anyoActual,
+						campo: JSON.stringify({'codigo': '$codigo', 'denominacion': '$denominacion'}),
+						restriccion: "{\"periodos.a" + $scope.anyoActual + ".pendientes\":{\"$lt\":0}}"
+				});
+		}
+	]);
+})(angular);
