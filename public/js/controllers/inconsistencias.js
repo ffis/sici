1238865239c6eb99ad60f42e0be5d3693490a37e
Mario Loksly <loksly@gmail.com>
2015-02-02 10:08:56 +0100

    Less code is better code

--- public/js/controllers/inconsistencias.js
+++ public/js/controllers/inconsistencias.js
@@ -1,28 +1,19 @@
-(function(angular, $, document, Blob, saveAs){
+(function(angular){
 	'use strict';
 	angular.module('sici')
-	.controller('InconsistenciasCtrl', ['$rootScope', '$scope', '$routeParams', 'Raw', 'Aggregate', '$timeout',
-		function ($rootScope, $scope, $routeParams, Raw, Aggregate, $timeout) {
-
+	.controller('InconsistenciasCtrl', ['$rootScope', '$scope', '$routeParams', 'Raw', 'Aggregate',
+		function ($rootScope, $scope, $routeParams, Raw, Aggregate) {
+			$scope.inconsistencias = Raw.query({model: 'reglasinconsistencias'}, function(){ $scope.update(); });
 			$rootScope.nav = 'inconsistencias';
 			$scope.oneAtATime = true;
 			$scope.camposamostrar = ['codigo', 'denominacion' ];
 			$scope.camposmostrados = $scope.camposamostrar;
-			$scope.inconsistencias = Raw.query({model: 'reglasinconsistencias'}, function(){ $scope.update(); });
 			$scope.seleccionados = {};
 			$scope.camposamostrar.forEach(function(campo){
 				$scope.seleccionados[campo] = $scope.camposmostrados.indexOf(campo) >= 0;
 			});
 			$scope.anualidad = new Date().getFullYear();
 			$scope.$watch('seleccionados.$', function(){ $scope.update(); });
-
-			$scope.exportXLS = function(idx){
-				var blob = new Blob(
-					['<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><table width="100%">' + document.getElementById('tabladatos' + idx).innerHTML + '</table>'], {
-					type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
-				});
-				saveAs(blob, "Report.xls");
-			};
 
 			$scope.update = function(){
 				$scope.inconsistencias.forEach(function(inconsistencia, i){
@@ -31,22 +22,9 @@
 					{
 						c[campoM] = '$' + campoM;
 					}
-
-					$scope.inconsistencias[i].datos = Aggregate.query({anualidad: $scope.anualidad, campo: JSON.stringify(c), restriccion: inconsistencia.restriccion},
-						function(){
-							$timeout(function() {
-								angular.element("[data-badge]").each(function(){
-									var a = angular.element( this ).find('.panel-heading a');
-									var html = '<span class="badge pull-right">' + $(this).data('badge') + '</span>';
-									if (angular.element( this ).find('.badge').length === 0 && angular.element( this ).find('tr').length > 0){
-										a.append(html);
-									}
-								});
-							}, 500);
-						}
-					);
+					$scope.inconsistencias[i].datos = Aggregate.query({anualidad: $scope.anualidad, campo: JSON.stringify(c), restriccion: inconsistencia.restriccion} );
 				});
 			};
 		}
 	]);
-})(angular, $, document, Blob, saveAs);
+})(angular);
