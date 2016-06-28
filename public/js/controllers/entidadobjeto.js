9021dd81bb16f4b23e31553513931209d42ef547
Loksly <loksly@gmail.com>
2016-06-28 11:55:25 +0200

    EntidadObjeto has better UI/UX

--- public/js/controllers/entidadobjeto.js
+++ public/js/controllers/entidadobjeto.js
@@ -7,9 +7,32 @@
 				$rootScope.setTitle('Entidad Objeto');
 				$scope.entidades = false;
 				$scope.filtro = '';
-				$scope.objetivostats = ObjetivoStats.query();
-				$scope.indicadores = {};
+				$scope.formulas = {};
+				$scope.objetivos = {};
+				$scope.setFormulasStats = function(){
+					for (var i = 0, j = $scope.objetivostats.length; i < j; i++){
+						if (typeof $scope.formulas[ $scope.objetivostats[i]._id ] === 'undefined'){
+							$scope.formulas[ $scope.objetivostats[i]._id ] = { formsok: 0, forms: 0};
+						}
+						if (typeof $scope.objetivos[ $scope.objetivostats[i]._id ] === 'undefined'){
+							$scope.objetivos[ $scope.objetivostats[i]._id ] = 0;
+						}
 
+						$scope.objetivos[ $scope.objetivostats[i]._id] += $scope.objetivostats[i].count;
+
+						for (var k = 0, l = $scope.objetivostats[i].formulas.length; k < l; k++){
+							for (var q = 0, w = $scope.objetivostats[i].formulas[k].length; q < w; q++ ){
+								if ($scope.objetivostats[i].formulas[k][q] !== '[]'){
+									$scope.formulas[ $scope.objetivostats[i]._id ].formsok++;
+								}
+								$scope.formulas[ $scope.objetivostats[i]._id ].forms++;
+							}
+						}
+					}
+				};
+				$scope.objetivostats = ObjetivoStats.query($scope.setFormulasStats);
+				$scope.indicadores = {};
+/*
 				$scope.getCount = function(_id){
 					for (var i = 0, j = $scope.objetivostats.length; i < j; i++){
 						if ($scope.objetivostats[i]._id === _id){
@@ -17,39 +40,22 @@
 						}
 					}
 					return 0;
-				};
+				};*/
 				$scope.anualidades = [];
 				for (var anualidad = 2015, maxanualidad = new Date().getFullYear(); anualidad <= maxanualidad; anualidad++ ){
 					$scope.anualidades.push({label: anualidad, value: 'a' + anualidad});
 				}
 				$scope.anualidad = $scope.anualidades[ $scope.anualidades.length - 1];
-				$scope.getFormulasStats = function(_id){
-					for (var i = 0, j = $scope.objetivostats.length; i < j; i++){
-						if ($scope.objetivostats[i]._id === _id){
-							var formsOK = 0, forms = 0;
-							for (var k = 0, l = $scope.objetivostats[i].formulas.length; k < l; k++){
-								for (var q = 0, w = $scope.objetivostats[i].formulas[k].length; q < w; q++ ){
-									if ($scope.objetivostats[i].formulas[k][q] !== '[]'){
-										formsOK++;
-									}
-									forms++;
-								}
-							}
-							return formsOK + '/' + forms;
-						}
-					}
-					return '0/0';
-				};
 
 				$scope.getIndicadoresStats = function(idjerarquia, anualidad){
-					if (!idjerarquia || typeof $scope.indicadores[idjerarquia] === 'undefined'){ return '';}
+					if (!idjerarquia || typeof $scope.indicadores[idjerarquia] === 'undefined'){ return 0;}
 					var indicadoresOK = 0;
 					for (var i = 0, j = $scope.indicadores[idjerarquia].length; i < j; i++){
 						if ($rootScope.isIndicadorCumplimentado($scope.indicadores[idjerarquia][i], anualidad)){
 							indicadoresOK++;
 						}
 					}
-					return indicadoresOK + '/' + $scope.indicadores[idjerarquia].length;
+					return indicadoresOK;
 				};
 
 				$scope.planesmejora = {};
@@ -75,7 +81,7 @@
 						$scope.acciones[anualidad] = {};
 					}
 					if (typeof $scope.acciones[anualidad][entidadobjeto._id] === 'undefined'){
-						$scope.acciones[anualidad][entidadobjeto.idjerarquia] = [];
+						$scope.acciones[anualidad][entidadobjeto._id] = [];
 					}
 					if (entidadobjeto.idjerarquia === 0){
 						$scope.planesmejora[anualidad][ entidadobjeto._id] = [];
@@ -100,8 +106,8 @@
 					});
 				};
 				$scope.cargaJerarquia = function(idjerarquia){
-					if (!idjerarquia){ return; }
-					if (parseInt(idjerarquia) <= 0 ){ return; }
+					if (!idjerarquia){ $scope.indicadores[0] = []; return; }
+					if (parseInt(idjerarquia) <= 0 ){ $scope.indicadores[0] = []; return; }
 					idjerarquia = parseInt(idjerarquia);
 					if (typeof $scope.jerarquias['' + idjerarquia] === 'undefined'){
 						$scope.jerarquias['' + idjerarquia] = Jerarquia.get({id: idjerarquia});
