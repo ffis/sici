7a516efec908f561b7b81d82ec042b83212ddcc2
loksly <loksly@gmail.com>
2017-02-27 12:19:35 +0100

    Refactor code

--- api/etiqueta.js
+++ api/etiqueta.js
@@ -5,25 +5,39 @@
 		return function(req, res){
 			var etiqueta = models.etiqueta();
 			var id = req.params._id;
-			if (id)
-			{
+			if (id){
 				etiqueta.findOne({'_id': id}, function(err, data){
 					if (err){
-						console.error(err); res.status(400).end();
+						console.error(err);
+						res.status(400).json({'error': 'An error has occurred'});
 					} else if (!data) {
 						res.status(404).end();
 					} else {
 						res.json(data);
 					}
 				});
-			}else{
+			} else {
 				etiqueta.find({}, function(err, data){
-					if (err) { console.error(err); res.status(500); res.end(); return ; }
-					res.json(data);
+					if (err){
+						console.error(err);
+						res.status(500).json({'error': 'An error has occurred'});
+					} else {
+						res.json(data);
+					}
 				});
 			}
 		};
 	};
+
+	function updateHandler(content){
+		return function(err){
+			if (err){
+				res.status(400).json({'error': 'An error has occurred'});
+			} else {
+				res.json(content);
+			}
+		};
+	}
 
 	module.exports.updateEtiqueta = function(models){
 		return function(req, res) {
@@ -31,13 +45,7 @@
 			var id = req.params.id;
 
 			var content = req.body;
-			etiqueta.update({'_id': id}, content, { upsert: true }, function(e){
-				if (e){
-					res.status(400).json({'error': 'An error has occurred'});
-				}else{
-					res.json(content);
-				}
-			});
+			etiqueta.update({'_id': id}, content, { upsert: true }, updateHandler(content));
 		};
 	};
 
@@ -45,13 +53,7 @@
 		return function(req, res) {
 			var Etiqueta = models.etiqueta();
 			var content = req.body;
-			new Etiqueta(content).save( function(e){
-				if (e){
-					res.status(400).json({'error': 'An error has occurred'});
-				}else{
-					res.json(content);
-				}
-			});
+			new Etiqueta(content).save( updateHandler(content) );
 		};
 	};
 
@@ -60,13 +62,7 @@
 			var etiqueta = models.etiqueta();
 			var id = req.params.id;
 			var content = req.body;
-			etiqueta.remove({'_id': id}, function(e){
-				if (e){
-					res.status(400).json({'error': 'An error has occurred'});
-				}else{
-					res.json(content);
-				}
-			});
+			etiqueta.remove({'_id': id}, updateHandler(content) );
 		};
 	};
 })(module);
