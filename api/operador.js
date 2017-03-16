08e2461416b8a9960270fee24bc804a019c5b02b
loksly <loksly@gmail.com>
2017-03-16 10:27:13 +0100

    added check for superuser permission

--- api/operador.js
+++ api/operador.js
@@ -4,7 +4,7 @@
 	module.exports.getOperador = function(req, res){
 		const operadormodel = req.metaenvironment.models.operador();
 		if (typeof req.params.id === 'string' && req.params.id !== ''){
-			operadormodel.findOne({'_id': req.metaenvironment.models.objectId(req.params._id)}).then(req.eh.okHelper(res, true), req.eh.errorHelper(res));
+			operadormodel.findOne({'_id': req.metaenvironment.models.objectId(req.params.id)}).then(req.eh.okHelper(res, true), req.eh.errorHelper(res));
 		} else {
 			operadormodel.find({}, req.eh.cb(res));
 		}
@@ -12,29 +12,40 @@
 
 	module.exports.updateOperador = function(req, res) {
 		if (typeof req.params.id === 'string' && req.params.id !== ''){
-			const operadormodel = req.metaenvironment.models.operador(),
-				content = JSON.parse(JSON.stringify(req.body));
-			Reflect.deleteProperty(content, '_id');
-			
-			operadormodel.update({'_id': req.metaenvironment.models.objectId(req.params.id)}, content, {upsert: true}, req.eh.cbWithDefaultValue(res, req.body));
+			if (req.user && req.user.permisoscalculados && req.user.permisoscalculados.superuser){
+				const operadormodel = req.metaenvironment.models.operador(),
+					content = JSON.parse(JSON.stringify(req.body));
+				Reflect.deleteProperty(content, '_id');
+				
+				operadormodel.update({'_id': req.metaenvironment.models.objectId(req.params.id)}, content, {upsert: true}, req.eh.cbWithDefaultValue(res, req.body));
+			} else {
+				req.eh.unauthorizedHelper(res);
+			}
 		} else {
 			req.eh.missingParameterHelper(res, 'id');
 		}
 	};
 
 	module.exports.newOperador = function(req, res) {
-		const Operador = req.metaenvironment.models.operador(),
-			content = req.body;
-		new Operador(content).save(req.metaenvironment.expresshelper.cbDefaultValue(res, content));
+		if (req.user && req.user.permisoscalculados && req.user.permisoscalculados.superuser){
+			const operadormodel = req.metaenvironment.models.operador(),
+				content = req.body;
+			operadormodel.create(content, req.metaenvironment.expresshelper.cbDefaultValue(res, content));
+		} else {
+			req.eh.unauthorizedHelper(res);
+		}
 	};
 
 	module.exports.removeOperador = function(req, res) {
-		const operadormodel = req.metaenvironment.models.operador(),
-			id = req.params.id;
-		if (typeof id === 'string' && id !== ''){
-			operadormodel.remove({'_id': req.metaenvironment.models.objectId(req.params.id)}, req.eh.cbWithDefaultValue(res, {}));
+		if (req.user && req.user.permisoscalculados && req.user.permisoscalculados.superuser){
+			const operadormodel = req.metaenvironment.models.operador();
+			if (typeof req.params.id === 'string' && req.params.id !== ''){
+				operadormodel.remove({'_id': req.metaenvironment.models.objectId(req.params.id)}, req.eh.cbWithDefaultValue(res, {}));
+			} else {
+				req.eh.missingParameterHelper(res, 'id');
+			}
 		} else {
-			req.eh.missingParameterHelper(res, 'id');
+			req.eh.unauthorizedHelper(res);
 		}
 	};
 
